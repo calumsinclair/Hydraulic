6d86db9123561b4c2070afa5ccb95ef19ff4c463
Unknown <c.matear@gmail.com>
2021-08-21 17:19:32 +0100

    x

--- Hydraulic/HydraulicComponents/TestModel.cs
+++ Hydraulic/HydraulicComponents/TestModel.cs
@@ -6,24 +6,10 @@ namespace Hydraulic.HydraulicComponents
 {
     public class TestModel: NodeModel
     {
-        public List<PortModel> ports = new List<PortModel>();
         public double FirstNumber { get; set; }
         public double SecondNumber { get; set; }
 
-        public TestModel(Point position) : base(position) 
-        {
-            
-            PortModel port1 = new PortModel("One", this);
-            PortModel port2 = new PortModel("Two", this);
-            PortModel port3 = new PortModel("Three", this);
-            PortModel port4 = new PortModel("Four", this);
-
-            ports.Add(port1);
-            ports.Add(port2);
-            ports.Add(port3);
-            ports.Add(port4);
-
-        }
+        public TestModel(Point position) : base(position) { }
 
     }
 }
