0bcc22e8ee77c51282873f14caa8c319a5107006
WINDOWS-LEAFPGB\calum <calum_sinclair@hotmail.com>
2021-08-21 17:06:32 +0100

    hi

---
+++ Hydraulic/HydraulicComponents/TestModel.cs
@@ -0,0 +1,28 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using System.Collections.Generic;
+
+namespace Hydraulic.HydraulicComponents
+{
+    public class TestModel: NodeModel
+    {
+        public List<PortModel> ports;
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+
+        public TestModel(Point position = null) : base(position) 
+        {
+            PortModel port1 = new PortModel("One", this);
+            PortModel port2 = new PortModel("Two", this);
+            PortModel port3 = new PortModel("Three", this);
+            PortModel port4 = new PortModel("Four", this);
+
+            ports.Add(port1);
+            ports.Add(port2);
+            ports.Add(port3);
+            ports.Add(port4);
+
+        }
+
+    }
+}
