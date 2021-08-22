ae0af8413fdbdeee9a362e3d2f5c68794d12035a
Unknown <c.matear@gmail.com>
2021-08-22 19:40:21 +0100

    proto for buttons

---
+++ Hydraulic/AppEvents.cs
@@ -0,0 +1,20 @@
+﻿using Hydraulic.HydraulicComponents.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hydraulic
+{
+
+    public class AppEvents
+    {
+        public record ComponentProps(IProperties props, string image);
+        public event EventHandler<ComponentProps> ComponentClicked;
+
+        public virtual void OnComponentClicked(IProperties props, string image)
+        {
+            ComponentClicked?.Invoke(this, new ComponentProps(props, image));
+        }
+    }
+}
