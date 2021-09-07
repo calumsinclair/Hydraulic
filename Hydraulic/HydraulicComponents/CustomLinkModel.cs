17f27460ce555850aac1d322d1968f22e912a6ac
WINDOWS-LEAFPGB\calum <calum_sinclair@hotmail.com>
2021-09-07 20:16:55 +0100

    custom links

---
+++ Hydraulic/HydraulicComponents/CustomLinkModel.cs
@@ -0,0 +1,15 @@
+﻿using Blazor.Diagrams.Core.Models;
+using Hydraulic.HydraulicComponents.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hydraulic.HydraulicComponents
+{
+    public class CustomLinkModel : LinkModel 
+    {
+        public IProperties props = new Hose(10);
+        public CustomLinkModel(PortModel sourcePort, PortModel targetPort = null) : base(sourcePort, targetPort) { }
+    }
+}
