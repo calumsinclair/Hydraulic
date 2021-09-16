45dab3704335e96384c73ba824140de646d4b721
Unknown <c.matear@gmail.com>
2021-09-16 20:27:03 +0100

    Fix bug, cleanup Value field

--- Hydraulic/HydraulicComponents/ComponentRegister.cs
+++ Hydraulic/HydraulicComponents/ComponentRegister.cs
@@ -13,11 +13,11 @@ namespace Hydraulic.HydraulicComponents
     public class ComponentData
     {
         public Func<IProperties> props;
-        public ComponentDisplayState displayState;
+        public Func<ComponentDisplayState> displayState;
         public PortInfo ports;
 
         public ComponentData
-            (Func<IProperties> newProps, ComponentDisplayState displayState, PortInfo  newPorts)
+            (Func<IProperties> newProps, Func<ComponentDisplayState> displayState, PortInfo  newPorts)
         {
             props = newProps;
             this.displayState = displayState;
@@ -58,28 +58,32 @@ namespace Hydraulic.HydraulicComponents
 
             //Pump
             Register(
-                new ComponentData(() => new Pump(PumpType.FixedDisplacement),
-                new PumpDisplayState(imagePrefix, imagePrefix + "PumpFixedDisplacement.png"),
+                new ComponentData(
+                    () => new Pump(PumpType.FixedDisplacement),
+                    ()=> new PumpDisplayState(imagePrefix, imagePrefix + "PumpFixedDisplacement.png"),
                 top
 
             ));
 
             // ControlValve       
             Register(
-                new ComponentData(() => new DirectionalControlVavle(ControlValveType.ProportionalControl),
-                new ControlValveDisplayState(imagePrefix, imagePrefix + "DirectionalControlValve43.png"),
+                new ComponentData(
+                    () => new DirectionalControlVavle(ControlValveType.ProportionalControl),
+                    () => new ControlValveDisplayState(imagePrefix, imagePrefix + "DirectionalControlValve43.png"),
                 fourMiddle));
 
             // Hydraulic Cylinder
             Register(
-                new ComponentData(() => new ActuatorCylinder(50),
-                new ComponentDisplayState(imagePrefix + "ActuatorCylinder.png"),
+                new ComponentData(
+                    () => new ActuatorCylinder(50),
+                    () => new ComponentDisplayState(imagePrefix + "ActuatorCylinder.png"),
                 twoBottom));
 
             // Tank
             Register(
-                new ComponentData(() => new Tank(),
-                new ComponentDisplayState(imagePrefix + "Tank.png"),
+                new ComponentData(
+                    () => new Tank(),
+                    ()=> new ComponentDisplayState(imagePrefix + "Tank.png"),
                 top));
 
         }
